174e35a5b9f58500f15e7dbc794f64fecfb06696
45zz <bugro.home@gmail.com>
2020-11-06 13:24:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] g={"java","lesson","17"};
+        Integer [] b={-10,-5,-7};
+        Collection f= new Collection();
+        f.addArray(g);
+        f.addArray(b);
+        f.add(3.0);
+        f.getList();
+        System.out.println(f);
+        System.out.println(f.get(1));
+        System.out.println(f.get(10));
+
+        System.out.println(Solution.createArrayList(g));
+        System.out.println(Solution.createHashList(g));
+        System.out.println(Solution.createHashMap(g,b));
+        // f.addArray(new File());
+    }
+}
